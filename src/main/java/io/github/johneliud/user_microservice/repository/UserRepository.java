efc1779b5f295aff3bea09cd60cc4b0b8d98c8df
John <johneliud4@gmail.com>
2026-03-26 15:49:41 +0300

    feat(user-microservice): add UserRepository with JPA methods for username and email lookups.

---
+++ src/main/java/io/github/johneliud/user_microservice/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package io.github.johneliud.user_microservice.repository;
+
+import io.github.johneliud.user_microservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
