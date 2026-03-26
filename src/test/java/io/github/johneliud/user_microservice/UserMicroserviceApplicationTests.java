682e794464ca624cc4f480333d73b1b0ad683966
John <johneliud4@gmail.com>
2026-03-26 14:35:04 +0300

    initial commit.

---
+++ src/test/java/io/github/johneliud/user_microservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.johneliud.user_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
